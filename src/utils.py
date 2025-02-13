e25a50ea97bf9122274290e79168e61b816ac63d
Tolga <mail@tolgadur.com>
2025-02-13 19:56:31 +0000

    fix show image utlils func

--- src/utils.py
+++ src/utils.py
@@ -12,9 +12,11 @@ def show_image(image: torch.Tensor, label: int):
 def show_image_in_dataset():
     dataset = MnistDataset()
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1)
-    image, _, label = next(iter(dataloader))
+    image, _, input_seq, target_seq = next(iter(dataloader))
 
     # remove batch size dimension
     image = image.squeeze(0)
 
+    # Show first digit from the sequence (excluding start token)
+    label = input_seq[0, 1].item()
     show_image(image, label)
